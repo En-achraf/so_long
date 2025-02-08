7f433490e03ca7d1127f8cbf692e0d8f8b95ecd6
0ac_raf0dev <acennadi@student.1337.ma>
2025-02-08 15:49:13 +0100

    quick update

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include "includes/so_long.h"
 
-int main(int ac, char **av) {
+int check_work(char *av) {
     char *map;
 
+    if (ft_strncmp(valid_map(av), "not valid format", 15) == 0)
+        return (ft_print_error(2) , 1);
+    else
+        ft_print_error(3);
+
+    map = ft_read_map(av);
+    if (!map) {
+        return (1);
+    }
+    ft_putstr_fd(map, 1);
+    free(map);
+}
+
+int main(int ac, char **av) {
     if (ac != 2) {
         ft_print_error(1);
         return (0);
     }
-
-    if (ft_strncmp(valid_map(av[1]), "not valid format", 15) == 0)
-        return (ft_print_error(2) , 1);
-
-    map = ft_read_map(av[1]);
-    if (!map) {
-        return (1);
-    }
-
-    ft_putstr_fd(map, 1);
-    free(map);
+    check_work(av[1]);
     return (0);
 }
--- src/ft_error.c
+++ src/ft_error.c
@@ -6,8 +6,8 @@ void	ft_print_error(int num)
 		ft_putstr_fd("no map available to use, try again\n", 1);
 	else if (num == 0)
 		ft_putstr_fd("the process Done Successfully\n", 1);
-    else if(2)
+    else if(num == 2)
         ft_putstr_fd("not valid format\n", 1);
-    else if(3)
+    else if(num == 3)
         ft_putstr_fd("valid format\n", 1);
 }

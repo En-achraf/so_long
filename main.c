8de6ccf1c6a044559372a8cd50b0f47ff774c0e7
0ac_raf0dev <acennadi@student.1337.ma>
2025-02-08 16:09:52 +0100

    quick update

--- main.c
+++ main.c
@@ -1,26 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 16:00:03 by acennadi          #+#    #+#             */
+/*   Updated: 2025/02/08 16:09:07 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/so_long.h"
 
-int check_work(char *av) {
-    char *map;
+int	check_work(char *av)
+{
+	char	*map;
 
-    if (ft_strncmp(valid_map(av), "not valid format", 15) == 0)
-        return (ft_print_error(2) , 1);
-    else
-        ft_print_error(3);
-
-    map = ft_read_map(av);
-    if (!map) {
-        return (1);
-    }
-    ft_putstr_fd(map, 1);
-    free(map);
+	if (ft_strncmp(valid_map(av), "not valid format", 15) == 0)
+		return (ft_print_error(2), 1);
+	else
+		ft_print_error(3);
+	map = ft_read_map(av);
+	if (!map)
+	{
+		return (1);
+	}
+	ft_putstr_fd(map, 1);
+	free(map);
 }
 
-int main(int ac, char **av) {
-    if (ac != 2) {
-        ft_print_error(1);
-        return (0);
-    }
-    check_work(av[1]);
-    return (0);
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return (ft_print_error(1) ,0);
+	check_work(av[1]);
+	return (0);
 }
--- src/ft_error.c
+++ src/ft_error.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 16:00:08 by acennadi          #+#    #+#             */
+/*   Updated: 2025/02/08 16:07:28 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/so_long.h"
 
 void	ft_print_error(int num)
 {
-	if (num == 1)
-		ft_putstr_fd("no map available to use, try again\n", 1);
-	else if (num == 0)
+	if (num == 0)
 		ft_putstr_fd("the process Done Successfully\n", 1);
-    else if(num == 2)
-        ft_putstr_fd("not valid format\n", 1);
-    else if(num == 3)
-        ft_putstr_fd("valid format\n", 1);
+	else if (num == 1)
+		ft_putstr_fd("no map available to use, try again\n", 1);
+	else if (num == 2)
+		ft_putstr_fd("not valid format\n", 1);
+	else if (num == 3)
+		ft_putstr_fd("valid format\n", 1);
 }

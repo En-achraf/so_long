9006bf053f576003af51e1c7492c176e2172d4b9
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2025-02-10 10:23:19 +0100

    Update

--- src/ft_valid_map.c
+++ src/ft_valid_map.c
@@ -1,17 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_valid_map.c                                     :+:      :+:    :+:   */
+/*   validformat.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/08 17:22:39 by acennadi          #+#    #+#             */
-/*   Updated: 2025/02/08 17:22:40 by acennadi         ###   ########.fr       */
+/*   Created: 2025/02/08 16:00:01 by acennadi          #+#    #+#             */
+/*   Updated: 2025/02/08 16:22:48 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void	ft_valid_map(void)
+char	*valid_format(char *str)
 {
+	int		len;
+	char	*s;
+
+	s = ".bar";
+	len = ft_strlen(str);
+	if (len < 4)
+		return ("not valid format");
+	if (ft_strncmp(&str[len - 4], s, 4) != 0)
+		return ("not valid format");
+	return ("valid format");
 }

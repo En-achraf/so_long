2be2314e21738ba392383c4b4435b8d8f6a6d6b3
0ac_raf0dev <acennadi@student.1337.ma>
2025-02-15 15:52:05 +0100

    quick update

--- main.c
+++ main.c
@@ -6,48 +6,51 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 16:00:03 by acennadi          #+#    #+#             */
-/*   Updated: 2025/02/15 15:18:51 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/02/15 15:35:15 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
 
-char	*check_work(char *av)
+char	*check_work(char *av, int *width, int *height)
 {
 	char		*map;
 	t_window	data;
 
 	data.height = 0;
 	data.width = 0;
+
 	if (ft_strncmp(valid_format(av), "not valid format", 15) == 0)
 		return (ft_print_error(2), NULL);
 	else
 		ft_print_error(3);
+
 	map = ft_read_map(av, &data.width, &data.height);
 	if (!map)
 	{
 		ft_putstr_fd("but file doesn't exist\n", 1);
 		return (NULL);
 	}
+	*width = data.width;
+	*height = data.height;
 	return (map);
 }
 
 int	main(int ac, char **av)
 {
 	char	*ptr;
-	t_window data;
+	int		width, height;
 
 	if (ac != 2)
 	{
 		ft_print_error(1);
 		return (0);
 	}
-	ptr = check_work(av[1]);
+
+	ptr = check_work(av[1], &width, &height);
 	if (!ptr)
-	{
-		return (0);
-		free(ptr);
-	}
-	ft_valid_map(ptr, data.width, data.height);
+		return (free(ptr), 0);
+	ft_valid_map(ptr, width, height);
+	free(ptr);
 	return (0);
 }
--- src/ft_valid_map.c
+++ src/ft_valid_map.c
@@ -6,73 +6,78 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:35:04 by acennadi          #+#    #+#             */
-/*   Updated: 2025/02/15 15:18:15 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/02/15 15:51:57 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void ft_free_grad(char **grad, int height) {
-    int i;
-    
-    i = 0;
-    while (i < height)
-     free(grad[i++]);
-    free(grad);
+void	ft_free_grad(char **grad, int height)
+{
+	int	i;
+
+	if (!grad)
+		return ;
+	for (i = 0; i < height; i++)
+		free(grad[i]);
+	free(grad);
 }
 
 static char	**ft_get_grad(char *str, int width, int height)
 {
 	char	**arry;
 
-	int(i), (j);
-	i = 0;
-	j = 0;
+	int i, j;
 	arry = malloc(height * sizeof(char *));
 	if (!arry)
-		return (free(arry), NULL);
-	while (i < height)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
 		arry[i] = malloc((width + 1) * sizeof(char));
 		if (!arry[i])
 		{
-			while (j < i)
-				free(arry[j++]);
-			free(arry);
+			ft_free_grad(arry, i);
 			return (NULL);
 		}
 		ft_strlcpy(arry[i], str + (i * (width + 1)), width + 1);
-		i++;
 	}
 	return (arry);
 }
 
-char **ft_valid_map(char *str, int width, int height) {
-    t_map data;
-    int (exit_count), (collectible_count), (player_count);
-	
-	collectible_count = 0;
-	player_count = 0;
-	exit_count = 0;
-    data.grad = ft_get_grad(str, width, height);
-    if (!data.grad) return (ft_putstr_fd("Error: Failed to create 2D map\n", 2), NULL);
+char	**ft_valid_map(char *str, int width, int height)
+{
+	t_map	data;
+	int		(exit_count), (collectible_count), (player_count);
+	char	c;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            char c = data.grad[y][x];
-            if (c == 'P') player_count++;
-            else if (c == 'C') collectible_count++;
-            else if (c == 'E') exit_count++;
-            else if (c != '0' && c != '1') {
-                ft_free_grad(data.grad, height);
-                return (ft_putstr_fd("Error: Invalid character in map\n", 2), NULL);
-            }
-        }
-    }
-
-    if (player_count != 1 || exit_count != 1 || collectible_count < 1) {
-        ft_free_grad(data.grad, height);
-        return (ft_putstr_fd("Error: Invalid player, exit, or collectible count\n", 2), NULL);
-    }
-    return data.grad;
+	exit_count = 0, collectible_count = 0, player_count = 0;
+	data.grad = ft_get_grad(str, width, height);
+	if (!data.grad)
+		return (ft_putstr_fd("Error: Failed to create 2D map\n", 2), NULL);
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			c = data.grad[y][x];
+			if (c == 'P')
+				player_count++;
+			else if (c == 'C')
+				collectible_count++;
+			else if (c == 'E')
+				exit_count++;
+			else if (c != '0' && c != '1')
+			{
+				ft_free_grad(data.grad, height); // ✅ Free before returning
+				return (ft_putstr_fd("Error: Invalid character in map\n", 2),
+					NULL);
+			}
+		}
+	}
+	if (player_count != 1 || exit_count != 1 || collectible_count < 1)
+	{
+		ft_free_grad(data.grad, height); // ✅ Free before returning
+		return (ft_putstr_fd("Error: Invalid player, exit, or collectible count\n", 2), NULL);
+	}
+	return (data.grad);
 }
+

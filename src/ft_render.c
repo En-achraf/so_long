894755d8f08cb3f236a6b9d8d44ff0d77dc14b8d
0ac_raf0dev <acennadi@student.1337.ma>
2025-02-18 20:38:52 +0100

    quick update

--- src/ft_render.c
+++ src/ft_render.c
@@ -6,11 +6,66 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:18:39 by acennadi          #+#    #+#             */
-/*   Updated: 2025/02/18 20:33:41 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/02/18 20:38:22 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+void	ft_init_game(t_game *game, char *map_path)
+{
+	game->win.mlx = mlx_init();
+	if (!game->win.mlx)
+		ft_error(ERR_MLX);
+	game->map.grad = ft_valid_map(map_path, &game->map.width,
+			&game->map.height);
+	if (!game->map.grad)
+		ft_error(ERR_MAP);
+	game->win.win = mlx_new_window(game->win.mlx, game->map.width * TILE_SIZE,
+			game->map.height * TILE_SIZE, "so_long");
+	if (!game->win.win)
+		ft_error(ERR_MLX);
+	game->wall.img = mlx_xpm_file_to_image(game->win.mlx, "textures/wall.xpm",
+			&game->wall.width, &game->wall.height);
+	game->floor.img = mlx_xpm_file_to_image(game->win.mlx, "textures/floor.xpm",
+			&game->floor.width, &game->floor.height);
+	game->player.img = mlx_xpm_file_to_image(game->win.mlx,
+			"textures/player.xpm", &game->player.width, &game->player.height);
+	game->collectible.img = mlx_xpm_file_to_image(game->win.mlx,
+			"textures/collectible.xpm", &game->collectible.width,
+			&game->collectible.height);
+	game->exit.img = mlx_xpm_file_to_image(game->win.mlx, "textures/exit.xpm",
+			&game->exit.width, &game->exit.height);
+	if (!game->wall.img || !game->floor.img || !game->player.img
+		|| !game->collectible.img || !game->exit.img)
+		ft_error(ERR_TEX);
+	game->map.moves = 0;
+	game->map.collectibles = 0;
+	game->map.exit = 0;
+}
+
+int	ft_close_game(t_game *game)
+{
+	if (game->win.mlx)
+	{
+		if (game->wall.img)
+			mlx_destroy_image(game->win.mlx, game->wall.img);
+		if (game->floor.img)
+			mlx_destroy_image(game->win.mlx, game->floor.img);
+		if (game->player.img)
+			mlx_destroy_image(game->win.mlx, game->player.img);
+		if (game->collectible.img)
+			mlx_destroy_image(game->win.mlx, game->collectible.img);
+		if (game->exit.img)
+			mlx_destroy_image(game->win.mlx, game->exit.img);
+		if (game->win.win)
+			mlx_destroy_window(game->win.mlx, game->win.win);
+		mlx_destroy_display(game->win.mlx);
+		free(game->win.mlx);
+	}
+	ft_free_grad(game->map.grad, game->map.height);
+	exit(0);
+}
 
 void	ft_render_map(const char *str)
 {
@@ -29,7 +84,7 @@ void	ft_render_map(const char *str)
 
 int	ft_render_frame(t_game *game)
 {
-	t_var (var);
+	t_var(var);
 	var.y = -1;
 	while (++var.y < game->map.height)
 	{

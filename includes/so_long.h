0bf9da6fa0a61b27b77e058de88a197d1eddb7f3
0ac_raf0dev <acennadi@student.1337.ma>
2025-03-04 16:56:43 +0000

    quick update

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 09:43:27 by acennadi          #+#    #+#             */
-/*   Updated: 2025/03/03 16:35:03 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/03/04 16:45:57 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ typedef struct s_map
 {
 	int		height;
 	int		width;
+	void	*image;
 	char	**grad;
 	char	**visited;
 	int		start_x;
@@ -65,7 +66,7 @@ void		ft_render(int width, int height);
 char		**ft_valid_map(char *str, int width, int height);
 int 	ft_check_map(char** grad, int width, int height);
 int		findItems(char **grad, int width, int height);
-
+t_var	find_position(char **grad, int row, int col, char target);
 
 // file reading
 char		*ft_read_file(char *str, int *width, int *height);
--- src/ft_render.c
+++ src/ft_render.c
@@ -6,21 +6,35 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:43:45 by acennadi          #+#    #+#             */
-/*   Updated: 2025/02/26 14:40:15 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/03/04 16:56:28 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+void *ft_load_textures(void *mlx, int num) {
+	t_var texture;
+	if(num == 0)
+		texture.map.image = mlx_xpm_file_to_image(mlx, "../textures/", 64, 64);
+
+	return (texture.map.image);
+}
+
+void ft_render_map(void *mlx, void *win)
+{
+	t_map	textures;
+
+	
+}
 
 void	ft_render(int width, int height)
 {
 	t_map data;
 
 	data.win.mlx = mlx_init();
-	data.win.win = mlx_new_window(data.win.mlx, width, height, "test");
+	data.win.win = mlx_new_window(data.win.mlx, width * TEXTURE_SIZE, height * TEXTURE_SIZE, "so_long");
 	if (!data.win.win)
-	{
 		mlx_destroy_window(data.win.mlx, data.win.win);
-	}
+	ft_render_map(data.win.mlx, data.win.win);
 	mlx_loop(data.win.mlx);
 }

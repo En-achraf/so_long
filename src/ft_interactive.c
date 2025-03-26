027bee5e5a798159d63644e08b2f4c424c7d0652
0ac_raf0dev <acennadi@student.1337.ma>
2025-03-26 17:11:16 +0000

    quick update

--- src/ft_interactive.c
+++ src/ft_interactive.c
@@ -3,92 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_interactive.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 15:19:04 by acennadi          #+#    #+#             */
-/*   Updated: 2025/03/24 20:21:11 by marvin           ###   ########.fr       */
+/*   Updated: 2025/03/26 17:10:33 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-void ft_count_collectibles(t_map *data) {
-    int x;
-    int y;
+void	ft_count_collectibles(t_map *data)
+{
+	int	x;
+	int	y;
 
-    y = 0;
-    while(data->height > y)
-    {
-        x = 0;
-        while(data->width > x) {
-            if(data->grad[y][x] == 'C')
-                data->collectibles++;
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (data->height > y)
+	{
+		x = 0;
+		while (data->width > x)
+		{
+			if (data->grad[y][x] == 'C')
+				data->collectibles++;
+			x++;
+		}
+		y++;
+	}
 }
 
-static void move_player(t_map *data, int new_x, int new_y)
+static void	move_player(t_map *data, int new_x, int new_y)
 {
-    if (data->grad[data->player.y][data->player.x] != 'E')
-        data->grad[data->end_y][data->end_x] = 'E';
-    if (data->grad[new_y][new_x] == 'C')
-        data->collectibles--;
-    if (data->grad[new_y][new_x] == 'E' && data->collectibles == 0)
-        ft_close_window(data);
-    else if (data->grad[new_y][new_x] == 'E' && data->collectibles != 0) {
-        ;
-    }
-    else
-    {
-        data->grad[data->player.y][data->player.x] = '0';
-        data->grad[new_y][new_x] = 'P';
-        data->player.x = new_x;
-        data->player.y = new_y;
-    }
+	if (data->grad[data->player.y][data->player.x] != 'E')
+		data->grad[data->end_y][data->end_x] = 'E';
+	if (data->grad[new_y][new_x] == 'C')
+		data->collectibles--;
+	if (data->grad[new_y][new_x] == 'E' && data->collectibles == 0)
+		ft_close_window(data);
+	else
+	{
+		data->grad[data->player.y][data->player.x] = '0';
+		data->grad[new_y][new_x] = 'P';
+		data->player.x = new_x;
+		data->player.y = new_y;
+	}
 }
 
-int keyhook(int key_code, t_map *data)
+int	keyhook(int key_code, t_map *data)
 {
-    int x = data->player.x;
-    int y = data->player.y;
+	int	x;
+	int	y;
 
-    if (key_code == ESC) 
-        ft_close_window(data);
-    else if ((key_code == KEY_W || key_code == KEY_UP) && y > 0 && data->grad[y - 1][x] != '1')
-        move_player(data, x, y - 1);
-    else if ((key_code == KEY_S || key_code == KEY_DOWN) && y < data->height - 1 && data->grad[y + 1][x] != '1')
-        move_player(data, x, y + 1);
-    else if ((key_code == KEY_A || key_code == KEY_LEFT) && x > 0 && data->grad[y][x - 1] != '1')
-        move_player(data, x - 1, y);
-    else if ((key_code == KEY_D || key_code == KEY_RIGHT) && x < data->width - 1 && data->grad[y][x + 1] != '1')
-        move_player(data, x + 1, y);
-    ft_render_map(data, data->player);
-    return (0);
+	x = data->player.x;
+	y = data->player.y;
+	if (key_code == ESC)
+		ft_close_window(data);
+	else if ((key_code == KEY_W || key_code == KEY_UP) && y > 0 && data->grad[y
+		- 1][x] != '1')
+		move_player(data, x, y - 1);
+	else if ((key_code == KEY_S || key_code == KEY_DOWN) && y < data->height - 1
+		&& data->grad[y + 1][x] != '1')
+		move_player(data, x, y + 1);
+	else if ((key_code == KEY_A || key_code == KEY_LEFT) && x > 0
+		&& data->grad[y][x - 1] != '1')
+		move_player(data, x - 1, y);
+	else if ((key_code == KEY_D || key_code == KEY_RIGHT) && x < data->width - 1
+		&& data->grad[y][x + 1] != '1')
+		move_player(data, x + 1, y);
+	ft_render_map(data, data->player);
+	return (0);
 }
 
-void ft_interactive(t_map *data)
+void	ft_interactive(t_map *data)
 {
-    t_var *position;
+	t_var	*position;
 
-    position = find_position(data->grad, data->height, data->width, 'P');
-    if (!position || position->x == -1 || position->y == -1 || 
-        position->x >= data->width || position->y >= data->height) {
-        free(position);
-        exit(1);
-    }
-    data->player.x = position->x;
-    data->player.y = position->y;
-    free(position);
-    position = find_position(data->grad, data->height, data->width, 'E');
-    if (!position || position->x == -1 || position->y == -1) {
-        free(position);
-        exit(1);
-    }
-    data->end_x = position->x;
-    data->end_y = position->y;
-    free(position);
-    ft_count_collectibles(data);
-    mlx_key_hook(data->win.win, keyhook, data);
+	position = find_position(data->grad, data->height, data->width, 'P');
+	if (!position || position->x == -1 || position->y == -1
+		|| position->x >= data->width || position->y >= data->height)
+	{
+		free(position);
+		exit(1);
+	}
+	data->player.x = position->x;
+	data->player.y = position->y;
+	free(position);
+	position = find_position(data->grad, data->height, data->width, 'E');
+	if (!position || position->x == -1 || position->y == -1)
+	{
+		free(position);
+		exit(1);
+	}
+	data->end_x = position->x;
+	data->end_y = position->y;
+	free(position);
+	ft_count_collectibles(data);
+	mlx_key_hook(data->win.win, keyhook, data);
 }
--- src/ft_valid_map.c
+++ src/ft_valid_map.c
@@ -6,47 +6,49 @@
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:56:38 by acennadi          #+#    #+#             */
-/*   Updated: 2025/03/26 16:23:30 by acennadi         ###   ########.fr       */
+/*   Updated: 2025/03/26 17:05:24 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int ft_check_bourders(char **map, int height, int width) {
-	int x;
-	int y;
+int	ft_check_bourders(char **map, int height, int width)
+{
+	int	x;
+	int	y;
 
 	y = 0;
-	while (y < height) 
+	while (y < height)
 	{
 		x = 0;
 		while (x < width)
 		{
 			if (map[y][width - 1] == '0' || map[y][0] == '0')
-				return (ft_putstr_fd("the map not surrounded by walls\n", 2), 1);	
+				return (ft_putstr_fd("the map not surrounded by walls\n", 2),
+					1);
 			x++;
 		}
 		y++;
 	}
-	return 0;
+	return (0);
 }
 
-int ft_check_character(char *str)
+int	ft_check_character(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] != 'E' && str[i] != 'P' && str[i] != 'C' 
-            && str[i] != '1' && str[i] != '0' && str[i] != '\n')
-            return (ft_pterr(3), 1); 
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != 'E' && str[i] != 'P' && str[i] != 'C' && str[i] != '1'
+			&& str[i] != '0' && str[i] != '\n')
+			return (ft_pterr(3), 1);
+		i++;
+	}
+	return (0);
 }
 
-char	**To_2D(char *str, int width, int height)
+char	**to_2d(char *str, int width, int height)
 {
 	t_var	data;
 
@@ -69,16 +71,16 @@ char	**To_2D(char *str, int width, int height)
 char	**ft_valid_map(char *str, int width, int height)
 {
 	t_var	data;
-	
+
 	data.count = ft_check_character(str);
-	if(data.count)
+	if (data.count)
 		return (NULL);
-	data.map.grad = To_2D(str, width, height);
+	data.map.grad = to_2d(str, width, height);
 	if (!data.map.grad)
 		return (NULL);
 	data.count = ft_check_bourders(data.map.grad, height, width);
 	if (data.count)
-		return (free(str) ,ft_free_grad(data.map.grad, height), NULL);
+		return (free(str), ft_free_grad(data.map.grad, height), NULL);
 	data.count = finditems(data.map.grad, width, height);
 	if (!data.count)
 		return (ft_pterr(4), ft_free_grad(data.map.grad, height), NULL);
